157510ab19f0d5b112158646f11a17501aabaee9
rea-leaf <1518300818@qq.com>
2025-04-02 10:16:20 +0800

    建表sql 调整

--- sql/auditlog.sql
+++ sql/auditlog.sql
@@ -1,49 +1,21 @@
--- Create table
 create table AUDIT_LOG
 (
-  id          NUMBER not null,
-  tablename   VARCHAR2(50) not null,
-  columnname  VARCHAR2(50),
-  primarykey  NUMBER,
-  parentid    NUMBER,
-  newvalue    VARCHAR2(520),
-  oldvalue    VARCHAR2(520),
-  operation   VARCHAR2(50),
-  createtime  DATE,
-  createclerk NUMBER
+    id          NUMBER not null,
+    tablename   VARCHAR2(128) not null,
+    columnname  VARCHAR2(128),
+    primarykey  VARCHAR2(24),
+    parentid    NUMBER,
+    newvalue    VARCHAR2(4000),
+    oldvalue    VARCHAR2(4000),
+    operation   VARCHAR2(50),
+    createtime  DATE,
+    createclerk VARCHAR2(24),
+    PRIMARY KEY (ID)
 )
-tablespace USERS
-  pctfree 10
-  initrans 1
-  maxtrans 255
-  storage
-  (
-    initial 64K
-    next 1M
-    minextents 1
-    maxextents unlimited
-  );
--- Create/Recreate primary, unique and foreign key constraints 
-alter table AUDIT_LOG
-  add constraint PK_AUDITLOG primary key (ID)
-  using index 
-  tablespace USERS
-  pctfree 10
-  initrans 2
-  maxtrans 255
-  storage
-  (
-    initial 64K
-    next 1M
-    minextents 1
-    maxextents unlimited
-  );
 
-
--- Create sequence 
 create sequence SEQ_AUDIT_LOG
-minvalue 1
-maxvalue 9999999999999999999999999999
-start with 381
-increment by 1
-cache 20;
+    minvalue 1
+    maxvalue 9999999999999999999999999999
+    start with 381
+    increment by 1
+    cache 20;
